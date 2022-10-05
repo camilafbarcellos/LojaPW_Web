d91d21534383f5529ba4dbc5b4767da1098cf7f6
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2022-10-05 00:18:52 -0300

    Personalização

--- src/main/java/br/edu/ifsul/dao/ClienteDAO.java
+++ src/main/java/br/edu/ifsul/dao/ClienteDAO.java
@@ -19,10 +19,10 @@ public class ClienteDAO<TIPO> extends DAOGenerico<Cliente> implements Serializab
         listaOrdem.add(new Ordem("id", "ID", "=")); // elemento 0
         listaOrdem.add(new Ordem("nome", "Nome", "like")); // elemento 1
         listaOrdem.add(new Ordem("cpf", "CPF", "like")); // elemento 2
-        listaOrdem.add(new Ordem("dataCad", "Data de Cadastro", "like")); // elemento 3
-        listaOrdem.add(new Ordem("email", "E-mail", "like")); // elemento 4
-        listaOrdem.add(new Ordem("telefone", "Telefone", "like")); // elemento 5
-        listaOrdem.add(new Ordem("dataNasc", "Data de Nascimento", "like")); // elemento 6
+        listaOrdem.add(new Ordem("email", "E-mail", "like")); // elemento 3
+        listaOrdem.add(new Ordem("telefone", "Telefone", "like")); // elemento 4
+        listaOrdem.add(new Ordem("dataNasc", "Data de Nascimento", "like")); // elemento 5
+        listaOrdem.add(new Ordem("dataCad", "Data de Cadastro", "like")); // elemento 6
         // definição da ordem atual
         ordemAtual = listaOrdem.get(1);
         converterOrdem = new ConverterOrdem();
